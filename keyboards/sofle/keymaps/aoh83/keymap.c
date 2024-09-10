12c3bc62b38bf5c23c0bbb7e3ce94efd4497a94a
Alexander Oh <oh.a@gmx.at>
2024-09-10 21:19:39 +0200

    add del key

--- keyboards/sofle/keymaps/aoh83/keymap.c
+++ keyboards/sofle/keymaps/aoh83/keymap.c
@@ -88,10 +88,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_LOWER] = LAYOUT(
   _______,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                         KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,
-  KC_GRV,  KC_EXLM,   KC_AT,  KC_LCBR, KC_RCBR, KC_GRV,                         _______, KC_7,    KC_8,    KC_9,    KC_0,  KC_F12,
+  KC_GRV,  KC_EXLM,   KC_AT,  KC_LCBR, KC_RCBR, KC_GRV,                         _______, KC_7,    KC_8,    KC_9,    KC_PAST,  KC_F12,
   _______, KC_HASH,  KC_DLR, KC_LPRN, KC_RPRN, KC_PIPE,                         _______, KC_4,    KC_5,    KC_6, KC_PLUS, KC_EQUAL,
   _______, KC_PERC,  KC_CIRC, KC_LBR, KC_RBR,  KC_TILD, _______,       _______, KC_AMPR,    KC_1,    KC_2,    KC_3, KC_BSLS, _______,
-                       _______, _______, _______, _______, _______,       _______, _______, KC_0, KC_DOT, _______
+                       _______, _______, _______, _______, KC_DEL,       _______, _______, KC_0, KC_DOT, _______
 ),
 /* RAISE
  * ,----------------------------------------.                    ,-----------------------------------------.
